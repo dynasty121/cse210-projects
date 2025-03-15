e7c9b3d1a1856549204613bf8761f6ae064dffb0
dynasti121 <donramond3@gmailL.com>
2025-03-14 18:04:42 -0700

    completed exercise 3 week 01

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,34 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Ask the user for the magic number
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        int guess = -1; // Initialize guess variable
+
+        // Loop until the user guesses correctly
+        while (guess != magicNumber)
+        {
+            // Ask the user for a guess
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            // Provide feedback to the user
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
